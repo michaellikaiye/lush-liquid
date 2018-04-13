7277ef4a37c9778c98d94f2fd3eed340a6efe322
amelendez19 <amelendez@stuy.edu>
2018-04-12 23:19:27 -0400

    hw97

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,8 +1,32 @@
 public class MazeSolver {
-    Maze maze;
-    public boolean Solver () {
-	int currank = maze.explorerposition.rank;
-	int curfile = maze.explorerposition.file;
-	return(maze.maze[currank][curfile] == 0);
+    Maze current;
+
+    public MazeSolver(Maze input) {
+	current = new Maze(input);
+    }
+    
+    public boolean solver () {
+	if (current.explorerIsOnA() == current.WALL)
+	    return false;
+	if (current.explorerIsOnA() == current.TREASURE)
+	    return true;
+	Maze snapShot = new Maze(current);
+	current.go(current.NORTH);
+	if (solver())
+	    return true;
+       	else current = new Maze(snapShot);
+	current.go(current.SOUTH);
+	if (solver())
+	    return true;
+       	else current = new Maze(snapShot);
+	current.go(current.EAST);
+	if (solver())
+	    return true;
+       	else current = new Maze(snapShot);
+	current.go(current.WEST);
+	if (solver())
+	    return true;
+	else current = new Maze(snapShot);
+	return false;
     }
 }
