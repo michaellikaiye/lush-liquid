d6340ccc582c1a2a49e3d1783807103ad6d2e3b8
michaellikaiye <mye1@stuy.edu>
2018-04-13 12:24:05 -0400

    hi

--- MazeSolver.java
+++ MazeSolver.java
@@ -4,29 +4,32 @@ public class MazeSolver {
     public MazeSolver(Maze input) {
 	current = new Maze(input);
     }
+    private final static int[] DIRECTIONS = {
+	Maze.EAST,
+	Maze.NORTH,
+	Maze.WEST,
+	Maze.SOUTH
+    };
     
     public boolean solver () {
-	if (current.explorerIsOnA() == current.WALL)
+	if (current.explorerIsOnA() == Maze.WALL)
 	    return false;
-	if (current.explorerIsOnA() == current.TREASURE)
+	if (current.explorerIsOnA() == Maze.TREASURE)
 	    return true;
-	Maze snapShot = new Maze(current);
-	current.go(current.NORTH);
-	if (solver())
-	    return true;
-       	else current = new Maze(snapShot);
-	current.go(current.SOUTH);
-	if (solver())
-	    return true;
-       	else current = new Maze(snapShot);
-	current.go(current.EAST);
-	if (solver())
-	    return true;
-       	else current = new Maze(snapShot);
-	current.go(current.WEST);
-	if (solver())
-	    return true;
-	else current = new Maze(snapShot);
+	for(int direction:DIRECTIONS) {
+	    Maze snapShot = new Maze(current);
+	    current.dropA(Maze.WALL);
+	    current.go(direction);
+	    
+	    System.out.println(current);
+	    if (solver())
+		return true;
+	    else
+		current = new Maze(snapShot);
+	    System.out.println(current);
+	}
+
 	return false;
     }
 }
+//enum
