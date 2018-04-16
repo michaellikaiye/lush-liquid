42337d1c84c216551cafac5a7405eabcee13b265
amelendez19 <34111575+amelendez19@users.noreply.github.com>
2018-04-15 22:38:02 -0400

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,3 +1,9 @@
+/**
+   Has-A Maze with the current position of the explorer.
+   Uses Maze representation to evaluate whether or not the explorer can reach 
+   the treasure.
+   Records number of moves through integer stepCounter.
+   */
 public class MazeSolver {
     Maze maze;
     public boolean Solver () {
